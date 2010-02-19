14c36245b45bc884ee658a571b84dc0eaaaf3e7f
akurzhan <akurzhan@a325a85d-3449-0410-964f-0d60b232b8d9>
2010-02-19 03:48:29 +0000

    updated constants

--- trunk/src/aurora/AuroraConstants.java
+++ trunk/src/aurora/AuroraConstants.java
@@ -11,7 +11,7 @@ package aurora;
  */
 public class AuroraConstants {
 	
-	public final static String COPYRIGHT = "Copyright (c) 2007-2009 TOPL Group at UC Berkeley";
+	public final static String COPYRIGHT = "Copyright (c) 2007-2010 TOPL Group at UC Berkeley";
 	public final static String CONTACT_EMAIL = "aurorarnm@googlegroups.com";
 	public final static String CONTACT_WWW = "http://path.berkeley.edu/topl";
 	public final static String DEFAULT_HOME = "c:\\tmp";
